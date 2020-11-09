23d993734ce7ed02eaeaa6cc2eee97fa7a6133e6
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-09 12:18:29 +0900

    use os stats

--- main.go
+++ main.go
@@ -3,32 +3,28 @@ package main
 import (
   "os"
   "fmt"
-  "io/ioutil"
   "github.com/spf13/cobra"
 )
 
 func main() {
   var cmdGenWithName = &cobra.Command{
-    Use:   "gen [Contest Name]",
+    Use:   "gen [Contest Name] [Extension]",
     Short: "Generate Files",
     Long: `Generate folders and files with the name you entered.`,
-    Args: cobra.MinimumNArgs(1),
+    Args: cobra.MinimumNArgs(2),
     Run: func(cmd *cobra.Command, args []string) {
-      var path, _ = os.Getwd()
-      files, err := ioutil.ReadDir(path)
+      info, err := os.Stat(args[0])
       if err != nil {
-          panic(err)
+        panic(err)
       }
-      var paths []string
-      for _, file := range files {
-        paths = append(paths, file.Name())
+
+      if info == nil{
+        fmt.Println(args[0] + " is already exits.")
+        return
       }
-      fmt.Println(paths)
-      for _, v := range paths {
-        if args[0] == v {
-          fmt.Println(args[0] + " is already exists.")
-          return
-        }
+      err = os.Mkdir(args[0], 0777);
+      if err != nil {
+        panic(err)
       }
     },
   }
