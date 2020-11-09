5698baa43e6fa5560c48bbaf645c3cd8fd596991
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-09 11:58:16 +0900

    get current directory

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+  "os"
+  "fmt"
+  "io/ioutil"
   "github.com/spf13/cobra"
 )
 
 func main() {
   var cmdGenWithName = &cobra.Command{
-    Use:   "Generate Files For [Contest Name]",
+    Use:   "gen [Contest Name]",
     Short: "Generate Files",
     Long: `Generate folders and files with the name you entered.`,
     Args: cobra.MinimumNArgs(1),
     Run: func(cmd *cobra.Command, args []string) {
-      
+      var path, _ = os.Getwd()
+      files, err := ioutil.ReadDir(path)
+      if err != nil {
+          panic(err)
+      }
+      var paths []string
+      for _, file := range files {
+        paths = append(paths, file.Name())
+      }
+      fmt.Println(paths)
     },
   }
 
