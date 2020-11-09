cf7016341a5fc15ea79c6b1352b7f26ef55f215c
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-11-09 12:45:26 +0900

    create files

--- main.go
+++ main.go
@@ -3,28 +3,35 @@ package main
 import (
   "os"
   "fmt"
+  "path/filepath"
   "github.com/spf13/cobra"
 )
 
 func main() {
+  var probList = []string {"A", "B", "C", "D", "E", "F"}
   var cmdGenWithName = &cobra.Command{
     Use:   "gen [Contest Name] [Extension]",
     Short: "Generate Files",
     Long: `Generate folders and files with the name you entered.`,
     Args: cobra.MinimumNArgs(2),
     Run: func(cmd *cobra.Command, args []string) {
-      info, err := os.Stat(args[0])
+      _, err := os.Stat(args[0])
+      if err == nil {
+          fmt.Println(args[0] + " is already exists.")
+          return
+      }
+
+      err = os.Mkdir(args[0], 0777);
       if err != nil {
         panic(err)
       }
 
-      if info == nil{
-        fmt.Println(args[0] + " is already exits.")
-        return
-      }
-      err = os.Mkdir(args[0], 0777);
-      if err != nil {
-        panic(err)
+      for _, v := range probList {
+        genFile, err := os.Create(filepath.Join("./", args[0], v + "." + args[1]))
+        if err != nil {
+          panic(err)
+        }
+        genFile.Close()
       }
     },
   }
